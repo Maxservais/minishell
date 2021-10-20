f8f2f6ad8a10490ebd07704bd2d2924e59b436a8
Maxime Servais <mservais@fu-r6-p5.s19.be>
2021-10-20 13:05:25 +0200

    reworked cd.c to send errors to STDERR

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,19 +1,29 @@
-#include "../minishell.h"
+/* Reminders:
+- Condition to check whether command is 'cd' or not can be removed
+once we check in the parsing which function should be called (cd, pwd, echo, etc.).
+- La commande 'cd' doit etre capable de gerer les variables qui ont été EXPORT.
+e.g. export test=Documents/minishell cd $test
+- the 'cd' command should update the PWD environment variable and create the OLDPWD env variable.
+e.g	bash-3.2$ env | grep PWD
+	PWD=/Users/mservais/Documents/minishell
+	bash-3.2$ cd pipex/
+	bash-3.2$ env | grep PWD
+	PWD=/Users/mservais/Documents/minishell/pipex
+	OLDPWD=/Users/mservais/Documents/minishell
+*/
 
-/* chdir() changes the current working directory of the calling
-process to the directory specified in path. */
+#include "../minishell.h"
 
 static void	error_cd(char *str)
 {
-	ft_putstr_fd("cd: ", 1);
-	ft_putstr_fd(strerror(errno), 1);
-	ft_putstr_fd(": ", 1);
-	ft_putendl_fd(str, 1);
+	ft_putstr_fd("cd: ", STDERR_FILENO);
+	ft_putstr_fd(strerror(errno), STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putendl_fd(str, STDERR_FILENO);
 }
 
 int	cd(char **splited)
 {
-	// THIS CONDITION CAN BE REMOVED ONCE WE CHECK IN THE PARSING WHICH FUNCTION TO CALL
 	if (!ft_strncmp(splited[0], "cd", 2)) 
 	{
 		if (!splited[1])
@@ -35,8 +45,3 @@ int	cd(char **splited)
 	}
 	return (EXIT_SUCCESS);
 }
-
-// LES ERREURS FAUT LES ECRIRE DANS LE STDOUT ET le STDERR? Ou juste STDOUT ?
-
-/// CA DOIT UPDATER LE PWD de env
-// CD doit etre capable de gerer les variables. 
