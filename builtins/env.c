a822c6d9222ef66e6b5e69109ed995ebd34aaa93
Maxime Servais <mservais@fu-r6-p5.s19.be>
2021-10-21 12:12:01 +0200

    env working

--- builtins/env.c
+++ builtins/env.c
@@ -1,37 +1,36 @@
+/* Reminders:
+- Condition to check whether command is 'env' or not can be removed
+once we check in the parsing which function should be called (cd, pwd, echo, etc.).
+*/
+
 #include "../minishell.h"
 
-void	env(char **splited)
+static void	error_env(char *str)
 {
+	ft_putstr_fd("env: ", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putendl_fd(strerror(errno), STDERR_FILENO);
+}
+
+int	env(char **splited)
+{
+	int			i;
+	extern char	**environ;
+
+	i = 0;
 	if (!ft_strncmp(splited[0], "env", 3))
 	{
-		printf("TERM_PROGRAM=%s\n", getenv("TERM_PROGRAM"));
-		printf("TERM=%s\n", getenv("TERM"));
-		printf("SHELL=%s\n", getenv("SHELL"));
-		printf("TMPDIR=%s\n", getenv("TMPDIR"));
-		printf("TERM_PROGRAM_VERSION=%s\n", getenv("TERM_PROGRAM_VERSION"));
-		printf("TERM_SESSION_ID=%s\n", getenv("TERM_SESSION_ID"));
-		printf("ZSH=%s\n", getenv("ZSH"));
-		printf("USER=%s\n", getenv("USER"));
-		printf("COMMAND_MODE=%s\n", getenv("COMMAND_MODE"));
-		printf("SSH_AUTH_SOCK=%s\n", getenv("SSH_AUTH_SOCK"));
-		printf("__CF_USER_TEXT_ENCODING=%s\n", getenv("__CF_USER_TEXT_ENCODING"));
-		printf("PAGER=%s\n", getenv("PAGER"));
-		printf("LSCOLORS=%s\n", getenv("LSCOLORS"));
-		printf("PATH=%s\n", getenv("PATH"));
-		printf("PWD=%s\n", getenv("PWD"));
-		printf("LANG=%s\n", getenv("LANG"));
-		printf("ITERM_PROFILE=%s\n", getenv("ITERM_PROFILE"));
-		printf("XPC_FLAGS=%s\n", getenv("XPC_FLAGS"));
-		printf("XPC_SERVICE_NAME=%s\n", getenv("XPC_SERVICE_NAME"));
-		printf("SHLVL=%s\n", getenv("SHLVL"));
-		printf("HOME=%s\n", getenv("HOME"));
-		printf("COLORFGBG=%s\n", getenv("COLORFGBG"));
-		printf("LC_TERMINAL_VERSION=%s\n", getenv("LC_TERMINAL_VERSION"));
-		printf("ITERM_SESSION_ID=%s\n", getenv("ITERM_SESSION_ID"));
-		printf("LESS=%s\n", getenv("LESS"));
-		printf("LOGNAME=%s\n", getenv("LOGNAME"));
-		printf("LC_TERMINAL=%s\n", getenv("LC_TERMINAL"));
-		printf("SECURITYSESSIONID=%s\n", getenv("SECURITYSESSIONID"));
-		printf("COLORTERM=%s\n", getenv("COLORTERM"));
+		if (splited[1])
+		{
+			error_env(splited[1]);
+			return (EXIT_FAILURE);
+		}
+		while (environ[i] != NULL)
+		{
+			ft_putendl_fd(environ[i], STDIN_FILENO);
+			i++;
+		}
 	}
+	return (EXIT_SUCCESS);
 }
--- minishell.h
+++ minishell.h
@@ -15,11 +15,10 @@
 #include "libft/libft.h"
 
 /* Minishell */
-void	export(char **splited);
-void	env(char **splited);
 
-void	ft_exit(char **splited, int *exit);
-void	echo(char **splited, int word_count, int quote);
+
+
+
 char	**handle_dquote(char *line, char **commands);
 char	**quote_remover(char **commands);
 int		count_occurence(char *str, char c);
@@ -28,5 +27,9 @@ int		occ_in_commands(char **commands, char c);
 /* Built-ins */
 int		pwd(char **splited);
 int		cd(char **splited);
+int		env(char **splited);
+void	export(char **splited);
+void	ft_exit(char **splited, int *exit);
+void	echo(char **splited, int word_count, int quote);
 
 #endif
